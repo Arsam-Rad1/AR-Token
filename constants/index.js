bb4ce9e5e87e308eb7dd32b22536a156765c8b80
Arsam Rad <140893761+Arsam-Rad1@users.noreply.github.com>
2023-07-31 11:10:10 +0200

    Update index.js
    Update Contract address

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8d0e68a256ed2fda0fd1e806d4a801e37ddc829b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
